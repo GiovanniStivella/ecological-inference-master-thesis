aaffacc17794b226ce49c8410a3c1e963d9336a1
GiovanniStivella <gstivella@outlook.it>
2026-04-14 19:46:46 +0200

    Setup of folders

---
+++ Code/Untitled.R
@@ -0,0 +1,20 @@
+#remotes::install_github("CoryMcCartan/seine")
+
+library(seine)
+data(elec_1968)
+
+spec = ei_spec(
+  elec_1968, 
+  predictors = vap_white:vap_other,
+  outcome = pres_dem_hum:pres_ind_wal, 
+  total = pres_total,
+  covariates = c(state, pop_city:pop_rural, farm:educ_coll, 
+                 inc_00_03k:inc_25_99k)
+)
+
+print(spec)
+
+m = ei_ridge(spec)
+rr = ei_riesz(spec, penalty = m$penalty)
+
+ei_est(regr = m, riesz = rr, data = spec, conf_level = 0.95)
