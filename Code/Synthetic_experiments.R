9f0f46514a1d72950ef55f9238bda425441a9f6f
GiovanniStivella <gstivella@outlook.it>
2026-06-18 11:38:26 +0200

    Generazione dataset sintetico e primi risultati

--- Code/Synthetic_experiments.R
+++ Code/Synthetic_experiments.R
@@ -14,41 +14,46 @@ high_income_South <- matrix(rtruncnorm(100, a=0, b=1, mean = 0.3, sd = 0.2))
 high_income <- as.data.frame(rbind(high_income_North, high_income_Centre, high_income_South))
 
 x <- as.matrix(cbind(high_income = high_income, low_income = 1-high_income))
+colnames(x) <- c("high_income", "low_income")
 
-#I firstly try without covariates
-
-#synthetic_dataset_no_covariates <- ei_synthetic(n=300, n_x = 1, x = high_income)
-
-#naive_no_covariates <- lm(y ~ x1, data = synthetic_dataset_no_covariates)
-
-#summary(naive_no_covariates)
-
-#I would like to add covariates
+#As the only covariate, I use geographical location
 area_yes <- matrix(1,100)
 area_no <- matrix(0,100)
 
 area_North <- rbind(area_yes, area_no, area_no)
 area_Centre <- rbind(area_no, area_yes, area_no)
-#area_South <- rbind(area_no, area_no, area_yes)
 
 area <- cbind(area_North, area_Centre)
+colnames(area) <- c("area_North", "area_Centre")
 
 covariates <- as.matrix(cbind(x, area))
 
-x <- as.matrix(cbind(high_income = covariates[, 1], low_income = 1 - covariates[, 1]))
+diff <- c(0,0.5,-0.3,0.1)
 
-z <- as.matrix(covariates[, 2:3, drop = FALSE])
+south <- as.matrix(c(0.6,0.2))
 
-synthetic_dataset <- ei_synthetic(x = x, z = z)
-synthetic_dataset[, c("z1", "z2")] <- z
-total <- matrix(100,300)
-synthetic_dataset <- cbind(synthetic_dataset, total)
+lambda <- matrix (diff, 2, 2)
+
+V12 <- as.matrix(covariates[, 3:4, drop = FALSE])
+eta <- t(apply(V12, 1, function(v) as.numeric(south + lambda %*% v)))
+colnames(eta) <- c("eta_high", "eta_low")
+
+beta <- matrix(rtruncnorm(a=0, b=1, n=600, mean = eta, sd = 0.05), nrow = 300, ncol = 2)
+colnames(beta) <- c("beta_high", "beta_low")
+
+covariates <- cbind(covariates, eta, beta)
+
+outcome <- rowSums(covariates[, 1:2] * covariates[, 7:8])
+
+total <- rep(1,300)
+
+synthetic_dataset <- as.data.frame(cbind(covariates, outcome, total))
 
 #I try and run the regressions on our synthetic dataset
 
 #The first idea is to run a linear regression
 
-naive <- lm(y~x1, data = synthetic_dataset)
+naive <- lm(outcome~high_income, data = synthetic_dataset)
 
 summary(naive)
 
@@ -56,15 +61,15 @@ summary(naive)
 
 synthspec <- ei_spec(
   synthetic_dataset, 
-  predictors = x1:x2,
-  outcome = y,
+  predictors = high_income:low_income,
+  outcome = outcome,
   total = total,
-  covariates = c(z1,z2)
+  covariates = c(area_North,area_Centre)
 )
 
 print(synthspec)
 
-synthm <- ei_ridge(synthspec)
-synthrr <- ei_riesz(synthspec, penalty = m$penalty)
+m <- ei_ridge(synthspec)
+rr <- ei_riesz(synthspec, penalty = m$penalty)
 
-ei_est(regr = synthm, riesz = synthrr, data = synthspec, conf_level = 0.95)
+ei_est(regr = m, riesz = rr, data = synthspec, conf_level = 0.95)
